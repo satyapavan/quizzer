eadf1086cd19084193239a22c665e81f431c757c
Satya Pavan <pavan.simham@gmail.com>
2020-10-01 00:11:42 +0530

    a major major rearchitecture changes

--- js/script.js
+++ js/script.js
@@ -1,92 +1,24 @@
 // TODO : GLOBAL variable? really??
 
-const quizContainer    = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton     = document.getElementById('submit');
-const timerContainer   = document.getElementById('timer');
+const quizContainer = document.getElementById('quiz');
 
 // pagination
-const previousButton = document.getElementById("previous");
 const nextButton = document.getElementById("next");
-let slides = document.querySelectorAll(".slide");
-let currentSlide = 0;
-let totalQuestions = 0;
-let timerProc;  // Is there a better way to deal with these than global parameters? TODO
 
 ////////////////////////////////////////////////////////////////////////////
 
-//    If we’re on the first slide, hide the Previous Slide button. Otherwise, show the button.
-//    If we’re on the last slide, hide the Next Slide button and show the Submit button. Otherwise, show the Next Slide button and hide the Submit button.
-
-function showSlide(n) {
-
-  if(typeof slides != 'undefined') {
-      slides[currentSlide].classList.remove('active-slide');
-  }
-  slides[n].classList.add('active-slide');
-  resultsContainer.innerHTML = (n+1) + ' of ' + totalQuestions + ' Question(s)';
-
-  currentSlide = n;
-  if(currentSlide===0){
-    previousButton.classList.remove('show-btn');
-    previousButton.classList.add('hide-btn');
-  }
-  else{
-    previousButton.classList.add('show-btn');
-    previousButton.classList.remove('hide-btn');
-  }
-  if(currentSlide===slides.length-1){
-    nextButton.classList.add('hide-btn');
-    nextButton.classList.remove('show-btn');
-
-    submitButton.classList.remove('hide-btn');
-    submitButton.classList.add('show-btn');
-  }
-  else{
-    nextButton.classList.remove('hide-btn');
-    nextButton.classList.add('show-btn');
-
-    submitButton.classList.add('hide-btn');
-    submitButton.classList.remove('show-btn');
-  }
-}
-
-function showNextSlide() {
-  showSlide(currentSlide + 1);
-}
-
-function showPreviousSlide() {
-  showSlide(currentSlide - 1);
-}
-
-function startTimer() {
-  var timer = 0, minutes, seconds;
-  timerProc = setInterval(function () {
-      minutes = parseInt(timer / 60, 10)
-      seconds = parseInt(timer % 60, 10);
-
-      minutes = minutes < 10 ? "0" + minutes : minutes;
-      seconds = seconds < 10 ? "0" + seconds : seconds;
-
-      timerContainer.textContent = 'Time Elapsed: ' + minutes + ":" + seconds;
-
-      timer++;
-
-  }, 1000);
-}
-
 function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
 }
 
 function loadData() {
 
   fetch('./questions/questions.json')
     .then(
-      function(response) {
+      function (response) {
         if (response.status !== 200) {
           console.log('Looks like there was a problem. Status Code: ' +
             response.status);
@@ -94,7 +26,7 @@ function loadData() {
         }
 
         // Examine the text in the response
-        response.json().then(function(json) {
+        response.json().then(function (json) {
           console.log(json);
           let loadedQuestions = json['quizzer']
           console.log('Inside loadData:', loadedQuestions)
@@ -105,125 +37,116 @@ function loadData() {
               console.log('BEFORE', currentQuestion);
 
               currentQuestion.question = decodeURIComponent(escape(window.atob(currentQuestion.question)));
+              currentQuestion.skill = currentQuestion.skill;
 
-              for(letter in currentQuestion.answers){
+              for (letter in currentQuestion.answers) {
                 currentQuestion.answers[letter] = decodeURIComponent(escape(window.atob(currentQuestion.answers[letter])));
               }
               console.log('AFTER', currentQuestion);
             }
           );
-          // TIL : This converts a local variable into a global variable. nice!
+          // Shuffle the questions so that the same questions are showed again and again
           shuffleArray(loadedQuestions);
-          window.quizQuest = loadedQuestions.slice(0, 9);
-          buildQuiz();
+
+          // TIL : This converts a local variable into a global variable. nice!
+          window.quizQuest = loadedQuestions; // .slice(0, 1);
+
+          showQuestion();
         });
       }
     )
-    .catch(function(err) {
+    .catch(function (err) {
       console.log('Fetch Error :-S', err);
     });
 }
 
-function buildQuiz(){
-  
+function showQuestion() {
+  // Get the latest question. Here shift pop's the first element out from array and returns it
+  window.currentQuestion = quizQuest.shift();
+  console.log('Inside showQuestion:', currentQuestion)
+
   // we'll need a place to store the HTML output
   const output = [];
 
-  console.log('Inside buildQuiz:', quizQuest)
-  //TODO : how is the value of questionNumber coming there? Interesting
-  // for each question...
-  quizQuest.forEach(
-    (currentQuestion, questionNumber) => {
+  // we'll want to store the list of answer choices
+  const answers = [];
 
-      // we'll want to store the list of answer choices
-      const answers = [];
+  // and for each available answer...
+  for (letter in currentQuestion.answers) {
+    // This ` here is called a string literal. its better than a string
+    // justify-content-between - is necessary to keep the span on the right end. else it moved to the left based on content
+    answers.push(
+      `<button type="button" class="btn btn-light btn-block text-left d-flex justify-content-between" 
+            id="option-${letter}" attr-value="${letter}">
+      <div>${currentQuestion.answers[letter]}</div>
+      <div><span id="span-${letter}"></span></div>
+          </button>`
+    );
+  }
 
-      // and for each available answer...
-      for(letter in currentQuestion.answers){
+  console.log(answers.join(''));
 
-        // ...add an HTML radio button
-        // This ` here is called a string literal. its better than a string
-        answers.push(
-          `<label>
-            <input type="radio" name="question_${questionNumber}" value="${letter}">
-            ${letter} :
-            ${currentQuestion.answers[letter]}
-          </label>`
-        );
-      }
-
-      totalQuestions++;
-      // add this question and its answers to the output
-      output.push(
-        `<div class="slide">
-            <div class="question"> ${currentQuestion.question} </div>
-            <div class="answers"> ${answers.join('')} </div>
-         </div>`
-      );
-    }
+  // add this question and its answers to the output
+  output.push(
+    `<div class="alert text-left mb-0 p-2" role="alert">
+    ${currentQuestion.question}
+    <h6 class="alert-heading font-italic text-right">#${currentQuestion.skill}</h6>
+    </div>
+        <hr class="mt-0">
+       ${answers.join('')}
+         `
   );
 
   // finally combine our output list into one string of HTML and put it on the page
   quizContainer.innerHTML = output.join('');
-  
-  slides = document.querySelectorAll(".slide");
-  showSlide(0);
 
-  startTimer();
+  ['a', 'b', 'c', 'd'].forEach((itr) => {
+    document.getElementById(`option-${itr}`).addEventListener("click", function () {
+      // Super simple logic to determine which button is clicked :-)
+      showResults(itr);
+    });
+  });
 }
 
-function showResults(){
+function showResults(selectedAnswer) {
+  console.log("The Selected Answer is : ", selectedAnswer);
+  console.log("The Correct  Answer is : ", currentQuestion.correctAnswer);
 
-  // gather answer containers from our quiz
-  const answerContainers = quizContainer.querySelectorAll('.answers');
-
-  // keep track of user's answers
-  let numCorrect = 0;
-
-  // for each question...
-  quizQuest.forEach( (currentQuestion, questionNumber) => {
-
-    // find selected answer
-    // TODO : Is this really going to be following same numbers? Better to use another set of hash mappings
-    const answerContainer = answerContainers[questionNumber];
-    const selector = 'input[name=question_'+questionNumber+']:checked';
-    const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-
-    // if answer is correct
-    if(userAnswer===currentQuestion.correctAnswer){
-      // add to the number of correct answers
-      numCorrect++;
-
-      //TODO : Add a CSS class not a direct style change. yuck!
-      // color the answers green
-      (answerContainer.querySelector(selector)).parentElement.classList.add('right-answer')
-    }
-    // if answer is wrong or blank
-    else{
-      // color the answers red
-      console.log( (answerContainer.querySelector(selector)) )
-      if( (answerContainer.querySelector(selector)) != null )
-        (answerContainer.querySelector(selector)).parentElement.classList.add('wrong-answer');
-      else
-        answerContainer.classList.add('wrong-answer');
-    }
+  ['a', 'b', 'c', 'd'].forEach((itr) => {
+    document.getElementById(`option-${itr}`).disabled = true;
   });
 
-  clearInterval(timerProc);
+  // if answer is correct, then color them green
+  if (selectedAnswer === currentQuestion.correctAnswer) {
+    document.getElementById(`option-${selectedAnswer}`).classList.replace('btn-light', 'btn-success');
 
-  // show number of correct answers out of total
-  let passPercent = ((numCorrect/totalQuestions) * 100).toFixed(0);
+    document.getElementById(`span-${selectedAnswer}`).innerHTML =
+      `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-check-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+  <path fill-rule="evenodd" d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"/>
+</svg>`
 
-  resultsContainer.innerHTML = passPercent + '% scored. ' + numCorrect + ' out of ' + quizQuest.length + ' answers are right.';
+  }
+  // if answer is wrong or blank color the answers red
+  else {
+    document.getElementById(`option-${selectedAnswer}`).classList.replace('btn-light', 'btn-outline-danger');
+    document.getElementById(`option-${currentQuestion.correctAnswer}`).classList.replace('btn-light', 'btn-outline-success');
+
+    document.getElementById(`span-${selectedAnswer}`).innerHTML =
+      `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-x-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+    <path fill-rule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+  </svg>`;
+
+    document.getElementById(`span-${currentQuestion.correctAnswer}`).innerHTML =
+      `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-check-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+  <path fill-rule="evenodd" d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"/>
+</svg>`
+  }
 }
 
-
-previousButton.addEventListener("click", showPreviousSlide);
-nextButton.addEventListener("click", showNextSlide);
+nextButton.addEventListener("click", showQuestion);
 
 // display quiz right away
 window.addEventListener("DOMContentLoaded", loadData);
-
-// on submit, show results
-submitButton.addEventListener('click', showResults);// Move this to a JSON file in later time
-
